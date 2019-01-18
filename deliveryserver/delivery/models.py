e92d2af72f218d5245d1e86822b2df6d1c9912e5
Megha Modi <meghamodi007@gmail.com>
2019-01-18 13:30:11 +0530

    models.py

--- deliveryserver/delivery/models.py
+++ deliveryserver/delivery/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+
+    USER_TYPE_CHOICES = (
+      (1, 'store_manager'),
+      (2, 'delivery_person'),
+    )
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name="user_profile"
+    )
+    user_type = models.IntegerField(
+        choices=USER_TYPE_CHOICES,
+        default=1
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+    modified_on = models.DateTimeField(
+        auto_now=True
+    )
+
+class DeliveryTask(models.Model):
+    PRIORITY_CHOICES = (
+        (1, 'high'),
+        (2, 'medium'),
+        (3, 'low'),
+    )
+    STATE_CHOICES = (
+        (1, 'new'),
+        (2, 'accepted'),
+        (3, 'completed'),
+        (4, 'declined'),
+        (5, 'cancelled'),
+    )
+    title = models.CharField(
+        max_length=255
+    )
+
+
+    priority = models.IntegerField(
+        choices=PRIORITY_CHOICES,
+        default=1
+    )
+    created_by = models.ForeignKey(
+        User,
+        related_name='delivery_tasks',
+        on_delete=models.CASCADE
+    )
+    state = models.IntegerField(
+        choices=STATE_CHOICES,
+        default=1
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
